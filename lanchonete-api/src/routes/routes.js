3c200d3e46ebb04457d4d6deae22f8defd5c02af
Marcos Vinicius Silva <MrcsVS@outlook.com>
2022-10-03 19:21:18 -0300

    habilitando cors e habilidando receber dados json

--- lanchonete-api/src/routes/routes.js
+++ lanchonete-api/src/routes/routes.js
@@ -1,5 +1,7 @@
-const router = require('express').Router
+const router = require('express').Router()
 
-
+router.get('/', (req, res) => {
+    res.send('Enviei uma resposta')
+})
 
 module.exports = {router}
--- lanchonete-api/src/server.js
+++ lanchonete-api/src/server.js
@@ -1,15 +1,36 @@
 const express = require('express')
+const cors = require('cors')
 
 const {router} = require('./routes/routes')
 const {serverInit} = require('./database/db')
 
 const app = express()
 
+// Permitindo requisições
+const allowedOrigins = [
+    'http://127.0.0.1:5500'
+]
+app.use(cors({
+    origin: function(origin, callback){
+        
+        // Permitindo requisições sem origin
+        if(!origin) return callback(null, true)
+
+        // Negando endereços
+        if(allowedOrigins.indexOf(origin) === -1) return callback(null, false)
+
+        return callback(null, true)
+    }
+}))
+
+// Habilita receber dados JSON
+app.use(express.json())
+
 // Conectando com a Database
 serverInit()
 
 // Configurando rotas
-app.use('/', router)
+app.use('/api', router)
 
 
 // Iniciando servidor

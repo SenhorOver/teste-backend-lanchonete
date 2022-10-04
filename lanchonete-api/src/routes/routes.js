df15349b0a2b3f9881ee08c1ff5602d4f59c5704
Marcos Vinicius Silva <MrcsVS@outlook.com>
2022-10-04 13:29:07 -0300

    adicionando rotas

--- lanchonete-api/src/routes/routes.js
+++ lanchonete-api/src/routes/routes.js
@@ -1,7 +1,10 @@
 const router = require('express').Router()
 
-router.get('/', (req, res) => {
-    res.send('Enviei uma resposta')
-})
+const {ClientController} = require('../controllers/clientController')
+
+router.get('/client/:id?', ClientController.get)
+router.post('/client', ClientController.post)
+router.put('/client/:id', ClientController.put)
+router.delete('/client/:id', ClientController.del)
 
 module.exports = {router}

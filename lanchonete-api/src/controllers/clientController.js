7b2e1d016ff334fd952298d2c52ab862aca23b47
Marcos Vinicius Silva <MrcsVS@outlook.com>
2022-10-07 14:09:51 -0300

    add client post - put - delete

--- lanchonete-api/src/controllers/clientController.js
+++ lanchonete-api/src/controllers/clientController.js
@@ -5,7 +5,7 @@ async function get(req, res){
     
     const search = id ? {_id: id} : null
 
-    const client = clientModel.find(search)
+    const client = await clientModel.find(search)
 
     const msg = client ? 'success' : 'error'
 
@@ -15,9 +15,42 @@ async function get(req, res){
     })
 }
 
+async function post(req, res){
+    const register = new clientModel(req.body)
 
+    await register.save()
 
+    res.send({
+        message: 'success'
+    })
+}
+
+async function put(req, res){
+    const {id} = req.params
+    const updated = await clientModel.findOneAndUpdate({_id: id}, req.body, {new: true})
+
+    const msg = updated ? 'success' : 'error'
+
+    res.send({
+        message: msg,
+        updated,
+    })
+}
+
+async function del(req, res){
+    const {id} = req.params
+    const removed = await clientModel.deleteOne({_id: id})
+
+    const msg = removed.acknowledged ? 'success' : 'error'
+
+    res.send({
+        message: msg,
+    })
+}
 
 module.exports = {
     get,
+    post,
+    put,
+    del
 }
--- lanchonete-api/src/routes/routes.js
+++ lanchonete-api/src/routes/routes.js
@@ -2,7 +2,15 @@ const router = require('express').Router()
 
 const clientController = require('../controllers/clientController')
 
+
+//Client CRUD
 router.get('/client/:id?', clientController.get)
+router.post('/client', clientController.post)
+router.put('/client/:id', clientController.put)
+router.delete('/client/:id', clientController.del)
+
+
+//Product CRUD
 
 
 

f2fc8ea5ce663be0f09623c88859898a20092a24
Marcos Vinicius Silva <MrcsVS@outlook.com>
2022-10-07 13:27:49 -0300

    começando do zero - nao gostei de como estava indo

--- lanchonete-api/src/models/admModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const squema = mongoose.Schema({
-    name: String,
-})
-
-const AdmModel = mongoose.model('administrator', squema)
-
-module.exports = {AdmModel}

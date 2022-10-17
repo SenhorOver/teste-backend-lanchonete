28d41be8d58423e1bfedb51538f8d54c429982c6
Marcos V. Silva <mrcsvs@outlook.com>
2022-10-17 16:04:43 -0300

    add html and css for loginPage after login has been successful and starting javascript

--- static-files/src/assets/modules/loginPage.js
+++ static-files/src/assets/modules/loginPage.js
@@ -1,7 +1,8 @@
 export const Main = {
     $btnRegister: document.querySelector('#btnRegister'),
     $btnLogin: document.querySelector('#btnLogin'),
-    divLogin: document.querySelector('.divLogin'),
+    $btnListOrder: document.querySelector('#btnListOrders'),
+    $btnCreateOrder: document.querySelector('#btnCreateOrder'),
     URL_API: 'http://localhost:8080/api/client',
 
     init(){
@@ -12,6 +13,8 @@ export const Main = {
     cacheSelectors(){
         this.$btnRegister.onclick = this.Events.click_Register.bind(this)
         this.$btnLogin.onclick = this.Events.click_Login.bind(this)
+        this.$btnListOrder.onclick = this.Events.click_listOrder.bind(this)
+        this.$btnCreateOrder.onclick = this.Events.click_createOrder.bind(this)
     },
 
     bindEvents(){
@@ -61,19 +64,39 @@ export const Main = {
             .then(response => response.json())
             .then(data => {
                 if(data.message === 'success'){
-                    this.fetchResponses.successLogin(data, this)
+                    this.fetchResponses.successLogin()
+                    this.fetchResponses.structureLayout(data)
                     return
                 }
                 alert('Deu erro')
             })
+        },
+
+        click_listOrder(e){
+            const ordersList = document.querySelector('.ordersList')
+
+            //Fazer fetch para orders, mandando o ID desse usuário que está no input hidden para a API para que ela faça a pesquisa no banco de dados e me devolva os pedidos desse cliente
+        },
+
+        click_createOrder(e){
+
         }
     },
 
     fetchResponses: {
-        successLogin(data, th){
-            console.log(data)
-            th.divLogin.classList.add('none')
-            th.divLogin.classList.remove('divLogin')
+        successLogin(){
+            const divLogin = document.querySelector('.divLogin')
+            divLogin.classList.add('none')
+            divLogin.classList.remove('divLogin')
+        },
+
+        structureLayout(data){
+            const lgMainTitle = document.querySelector('.lgMainTitle')
+            const loginFeito = document.querySelector('.logFeito')
+            const client = data.client[0]
+            
+            lgMainTitle.innerText = `Olá ${client.name}`
+            loginFeito.classList.remove('none')
         }
     }
 
